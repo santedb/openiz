cff52b7b49ed3c8f2bc14c4a198878efe92b53cb
Justin Fyfe <justin@fyfesoftware.ca>
2019-06-17 19:11:53 -0400

    Added subscription service scaffolding to OpenIZ
    (cherry picked from commit b5da6d1343d6e77fb9a920e0a5110a615bc30f78)

---
+++ OpenIZ.Core.Model/Subscription/SubscriptionServerDefinition.cs
@@ -0,0 +1,26 @@
+﻿using Newtonsoft.Json;
+using System.Xml.Serialization;
+
+namespace OpenIZ.Core.Model.Subscription
+{
+    /// <summary>
+    /// Represents a server subscription definition
+    /// </summary>
+    [XmlType(nameof(SubscriptionServerDefinition), Namespace = "http://openiz.org/subscription")]
+    public class SubscriptionServerDefinition
+    {
+
+        /// <summary>
+        /// Gets or sets the invariant name
+        /// </summary>
+        [JsonIgnore, XmlAttribute("invariant")]
+        public string InvariantName { get; set; }
+
+        /// <summary>
+        /// Gets or sets the SQL definition
+        /// </summary>
+        [XmlText, JsonIgnore]
+        public string Definition { get; set; }
+
+    }
+}
