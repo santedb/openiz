76ea26badfc453966bf5c0f0feec0aea84fea480
Justin Fyfe <justin@fyfesoftware.ca>
2019-06-18 13:47:11 -0400

    Revised version #s

--- OpenIZ.Messaging.IMSI.Client/Properties/AssemblyInfo.cs
+++ OpenIZ.Messaging.IMSI.Client/Properties/AssemblyInfo.cs
@@ -42,6 +42,6 @@ using System.Resources;
 //
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.3.0.*")][assembly: AssemblyInformationalVersion("Fredericton")]
-[assembly: AssemblyVersion("1.3.0.*")][assembly: AssemblyInformationalVersion("Fredericton")]
+// [assembly: AssemblyVersion("1.4.0.*")][assembly: AssemblyInformationalVersion("Fredericton")]
+[assembly: AssemblyVersion("1.4.0.*")][assembly: AssemblyInformationalVersion("Fredericton")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
